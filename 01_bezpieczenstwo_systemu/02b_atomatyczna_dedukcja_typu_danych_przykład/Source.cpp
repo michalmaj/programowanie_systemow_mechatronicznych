31401b378c809896546247dc2278e0ad59844ead
Michał Maj <michalpmaj@gmail.com>
2023-01-16 16:56:27 +0100

    automatyczna dedukcja typu danych przykład

--- 01_bezpieczenstwo_systemu/02b_atomatyczna_dedukcja_typu_danych_przykład/Source.cpp
+++ 01_bezpieczenstwo_systemu/02b_atomatyczna_dedukcja_typu_danych_przykład/Source.cpp
@@ -9,17 +9,24 @@ int main() {
     auto my_ints = { 1, 2, 3 };
     auto my_int_ptr = my_ints.begin();
 
-    std::map<int, std::string> my_map = { {1, std::string("four")}, {2, std::string("five")} };
+    std::map<int, std::string> my_map = {
+    	{1, std::string("four")}, {2, std::string("five")}
+    };
     auto my_map_begin = my_map.begin();
 
-    auto func = [](const std::string& a) { return a; };
+    auto func = [](const std::string& a)
+    {
+	    return a;
+    };
 
-    auto async_lambda = std::async([] { return std::string("Hello World"); });
+    auto async_lambda = std::async([] 
+        { return std::string("Hello World"); });
 
     auto start_timer = std::chrono::system_clock::now();
 
     auto pair = std::make_pair(1, std::string("one"));
 
-    auto tuple = std::make_tuple(std::string("Hello World"), 4.13f, 1, 22.16, false, 'z');
+    auto tuple = std::make_tuple(std::string("Hello World"), 
+        4.13f, 1, 22.16, false, 'z');
 
 }
