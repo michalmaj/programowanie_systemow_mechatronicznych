61cf06c0533a15a4ce7ea369c10f35f83a3ac8fe
Michał Maj <michalpmaj@gmail.com>
2023-01-17 18:42:46 +0100

    literały std::chrono

--- 01_bezpieczenstwo_systemu/03c_literały_std_chrono/Source.cpp
+++ 01_bezpieczenstwo_systemu/03c_literały_std_chrono/Source.cpp
@@ -17,17 +17,26 @@ int main() {
     auto schoolWay = 15min;
     auto homework = 2h;
 
-    auto schoolDayInSeconds = 2 * schoolWay + 6 * schoolHour + 4 * shortBreak + longBreak + homework;
+    auto schoolDayInSeconds = 
+        2 * schoolWay + 6 * schoolHour + 
+        4 * shortBreak + longBreak + homework;
 
-    std::cout << "School day in seconds: " << schoolDayInSeconds.count() << std::endl;
+    std::cout << "School day in seconds: " << 
+        schoolDayInSeconds.count() << std::endl;
 
-    std::chrono::duration<double, std::ratio<3600>> schoolDayInHours = schoolDayInSeconds;
-    std::chrono::duration<double, std::ratio<60>> schoolDayInMinutes = schoolDayInSeconds;
-    std::chrono::duration<double, std::ratio<1, 1000>> schoolDayInMilliseconds = schoolDayInSeconds;
+    std::chrono::duration<double, std::ratio<3600>>
+        schoolDayInHours = schoolDayInSeconds;
+    std::chrono::duration<double, std::ratio<60>>
+        schoolDayInMinutes = schoolDayInSeconds;
+    std::chrono::duration<double, std::ratio<1, 1000>>
+        schoolDayInMilliseconds = schoolDayInSeconds;
 
-    std::cout << "School day in hours: " << schoolDayInHours.count() << std::endl;
-    std::cout << "School day in minutes: " << schoolDayInMinutes.count() << std::endl;
-    std::cout << "School day in milliseconds: " << schoolDayInMilliseconds.count() << std::endl;
+    std::cout << "School day in hours: " << 
+        schoolDayInHours.count() << std::endl;
+    std::cout << "School day in minutes: " <<
+        schoolDayInMinutes.count() << std::endl;
+    std::cout << "School day in milliseconds: " << 
+        schoolDayInMilliseconds.count() << std::endl;
 
     std::cout << std::endl;
 
